0315eecaa59977a049107e32f2f1b2258ae2f2d6
kris1138 <contact@kris1138.com>
2014-05-15 22:17:27 +0100

    new team page
    mouse-over refined

--- IslandGIS/assets/js/show-hide.js
+++ IslandGIS/assets/js/show-hide.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){  
-$('#paulImg').click(function(){$('.blurb').hide(); $('#paul').show()});
-$('#jamesImg').click(function(){$('.blurb').hide(); $('#james').show()});
-$('#najyaImg').click(function(){$('.blurb').hide(); $('#najya').show()});
-$('#maxImg').click(function(){$('.blurb').hide(); $('#max').show()});
-$('#roshImg').click(function(){$('.blurb').hide(); $('#rosh').show()});
-$('#olgaImg').click(function(){$('.blurb').hide(); $('#olga').show()});
-$('#johnImg').click(function(){$('.blurb').hide(); $('#john').show()});
-$('#douglasImg').click(function(){$('.blurb').hide(); $('#douglas').show()});
-$('#mikeImg').click(function(){$('.blurb').hide(); $('#mike').show()});
+$('#paulImg').click(function(){$('.blurb').hide(); $('#paul').show(); $('.gallery').find('a').removeClass('active'); $('#paulImg').addClass('active')});
+$('#jamesImg').click(function(){$('.blurb').hide(); $('#james').show(); $('.gallery').find('a').removeClass('active'); $('#jamesImg').addClass('active')});
+$('#najyaImg').click(function(){$('.blurb').hide(); $('#najya').show(); $('.gallery').find('a').removeClass('active'); $('#najyaImg').addClass('active')});
+$('#maxImg').click(function(){$('.blurb').hide(); $('#max').show(); $('.gallery').find('a').removeClass('active'); $('#maxImg').addClass('active')});
+$('#roshImg').click(function(){$('.blurb').hide(); $('#rosh').show(); $('.gallery').find('a').removeClass('active'); $('#roshImg').addClass('active')});
+$('#olgaImg').click(function(){$('.blurb').hide(); $('#olga').show(); $('.gallery').find('a').removeClass('active'); $('#olgaImg').addClass('active')});
+$('#johnImg').click(function(){$('.blurb').hide(); $('#john').show(); $('.gallery').find('a').removeClass('active'); $('#johnImg').addClass('active')});
+$('#douglasImg').click(function(){$('.blurb').hide(); $('#douglas').show(); $('.gallery').find('a').removeClass('active'); $('#douglasImg').addClass('active')});
+$('#mikeImg').click(function(){$('.blurb').hide(); $('#mike').show(); $('.gallery').find('a').removeClass('active'); $('#mikeImg').addClass('active')});
 }); 
