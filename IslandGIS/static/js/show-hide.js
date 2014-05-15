0da1444590416962785e708cb72933aff4068b53
kris1138 <contact@kris1138.com>
2014-05-15 19:36:51 +0100

    updates for new team page

---
+++ IslandGIS/static/js/show-hide.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){  
+$('#paulImg').click(function(){$('.blurb').hide(); $('#paul').show()});
+$('#jamesImg').click(function(){$('.blurb').hide(); $('#james').show()});
+$('#najyaImg').click(function(){$('.blurb').hide(); $('#najya').show()});
+$('#maxImg').click(function(){$('.blurb').hide(); $('#max').show()});
+$('#roshImg').click(function(){$('.blurb').hide(); $('#rosh').show()});
+$('#olgaImg').click(function(){$('.blurb').hide(); $('#olga').show()});
+$('#johnImg').click(function(){$('.blurb').hide(); $('#john').show()});
+$('#douglasImg').click(function(){$('.blurb').hide(); $('#douglas').show()});
+$('#mikeImg').click(function(){$('.blurb').hide(); $('#mike').show()});
+});
